37df53c3878de0eb6068db643344375daea2fb48
bkcp <bkcp83@gmail.com>
2022-05-12 15:28:21 -0700

    Extends lets you inherit constructor properties from another class. Super lets you include constructor properties from the extended class  into a new constructor will createing a new object

--- app.js
+++ app.js
@@ -60,94 +60,139 @@
 
 // document.body.style.backgroundColor = color2.rgba(0.5);
 
-class Color{
-    //constructor executes immediately when a new color is created
-    constructor(r,g,b,name){
-        console.log("inside constructor");
-        console.log(r,g,b,name);
-        this.r = r;
-        this.g = g;
-        this.b = b;
-        this.name = name;
-        this.calcHSL();
-    }
-    greet(){
-        return `hello from ${this.name}`;
-    }
-    innerRGB(){
-        const{r,g,b} =  this;
-        return `${r},${g},${b}`
-    }
-    rgb(){
-        return `rgb(${this.innerRGB()})`
-    }
-    rgba(a =1.0){
-        return `rgba(${this.innerRGB()},${a})`;
-     }
-    hex(){
-        const {r,g,b} = this;
-        return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-    }
-    hsl(){
-        const {h,s,l} = this
-        return `hsl(${h},${s}%,${l}%)`
-    }
-    opposite(){
-        const {h,s,l} = this
-        const newHue = (h + 180) % 360; 
-        return `hsl(${newHue},${s}%,${l}%)`
-    }
-    fullSaturation(){
-        const{h,l} = this
-        return`hsl(${h},100%,${l}%)`
-    }
+
+
+// CLASSES
+// class Color{
+//     //constructor executes immediately when a new color is created
+//     constructor(r,g,b,name){
+//         console.log("inside constructor");
+//         console.log(r,g,b,name);
+//         this.r = r;
+//         this.g = g;
+//         this.b = b;
+//         this.name = name;
+//         this.calcHSL();
+//     }
+//     greet(){
+//         return `hello from ${this.name}`;
+//     }
+//     innerRGB(){
+//         const{r,g,b} =  this;
+//         return `${r},${g},${b}`
+//     }
+//     rgb(){
+//         return `rgb(${this.innerRGB()})`
+//     }
+//     rgba(a =1.0){
+//         return `rgba(${this.innerRGB()},${a})`;
+//      }
+//     hex(){
+//         const {r,g,b} = this;
+//         return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+//     }
+//     hsl(){
+//         const {h,s,l} = this
+//         return `hsl(${h},${s}%,${l}%)`
+//     }
+//     opposite(){
+//         const {h,s,l} = this
+//         const newHue = (h + 180) % 360; 
+//         return `hsl(${newHue},${s}%,${l}%)`
+//     }
+//     fullSaturation(){
+//         const{h,l} = this
+//         return`hsl(${h},100%,${l}%)`
+//     }
     
-    calcHSL() {
-		let { r, g, b } = this;
-		// Make r, g, and b fractions of 1
-		r /= 255;
-		g /= 255;
-		b /= 255;
+//     calcHSL() {
+// 		let { r, g, b } = this;
+// 		// Make r, g, and b fractions of 1
+// 		r /= 255;
+// 		g /= 255;
+// 		b /= 255;
 
-		// Find greatest and smallest channel values
-		let cmin = Math.min(r, g, b),
-			cmax = Math.max(r, g, b),
-			delta = cmax - cmin,
-			h = 0,
-			s = 0,
-			l = 0;
-		if (delta == 0) h = 0;
-		else if (cmax == r)
-			// Red is max
-			h = ((g - b) / delta) % 6;
-		else if (cmax == g)
-			// Green is max
-			h = (b - r) / delta + 2;
-		else
-			// Blue is max
-			h = (r - g) / delta + 4;
+// 		// Find greatest and smallest channel values
+// 		let cmin = Math.min(r, g, b),
+// 			cmax = Math.max(r, g, b),
+// 			delta = cmax - cmin,
+// 			h = 0,
+// 			s = 0,
+// 			l = 0;
+// 		if (delta == 0) h = 0;
+// 		else if (cmax == r)
+// 			// Red is max
+// 			h = ((g - b) / delta) % 6;
+// 		else if (cmax == g)
+// 			// Green is max
+// 			h = (b - r) / delta + 2;
+// 		else
+// 			// Blue is max
+// 			h = (r - g) / delta + 4;
 
-		h = Math.round(h * 60);
+// 		h = Math.round(h * 60);
 
-		// Make negative hues positive behind 360°
-		if (h < 0) h += 360;
-		// Calculate lightness
-		l = (cmax + cmin) / 2;
+// 		// Make negative hues positive behind 360°
+// 		if (h < 0) h += 360;
+// 		// Calculate lightness
+// 		l = (cmax + cmin) / 2;
 
-		// Calculate saturation
-		s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+// 		// Calculate saturation
+// 		s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
 
-		// Multiply l and s by 100
-		s = +(s * 100).toFixed(1);
-		l = +(l * 100).toFixed(1);
-		this.h = h;
-		this.s = s;
-		this.l = l;
-	}
+// 		// Multiply l and s by 100
+// 		s = +(s * 100).toFixed(1);
+// 		l = +(l * 100).toFixed(1);
+// 		this.h = h;
+// 		this.s = s;
+// 		this.l = l;
+// 	}
     
    
-}
-const pandaPink = new Color(255, 34, 0, 'pandaPink');
-const white = new Color(255,255,255, 'white');
+// }
+// const pandaPink = new Color(255, 34, 0, 'pandaPink');
+// const white = new Color(255,255,255, 'white');
 
-document.body.style.backgroundColor = pandaPink.opposite();
+// document.body.style.backgroundColor = pandaPink.opposite();
+
+
+//SUPER EXTENDS INHERITENCE
+class Pet{
+    constructor(name,age){
+        this.name = name;
+        this.age = age;
+        console.log('in pet constructor')
+
+    }
+    eat(){
+        return `${this.name} is Eating!`;
+    }
+}
+
+//extends keyword will use constructor 
+class Cat extends Pet{
+    constructor(name,age,livesLeft =9){
+        console.log("in cat constructor");
+        //super takes parameters from extended class
+        super(name,age);
+        this.livesLeft = livesLeft;
+    }
+    meow(){
+        return "meow"
+    }
+}
+
+class Dog extends Pet{
+    bark(){
+        return "woof"
+    }
+    //if the same method is in a class AND an extended class, this one will be used first, only uses extends method if not found initially
+    eat(){
+        return `${this.name}, wolf's down it's food`
+    }
+}
+
+const kitty = new Cat('kitty',9,2);
+console.log(kitty.meow());
+const pupper = new Dog('pup',4);
+console.log(pupper.bark());
